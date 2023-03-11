7a733327aff74afbf15060e506af4b6f266aedeb
Balint Korosi <balint.j.korosi@gmail.com>
2023-03-11 08:39:23 +0100

    yolo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Card from "@/components/Card";
+
 export default function Home() {
   return (
-    <div className="flex max-w-4xl mx-auto border-4 border-emerald-500">
-      text
+    <div className="flex max-w-4xl mx-auto mt-4 gap-6 ">
+      <div className="w-1/3">
+        <Card>test links</Card>
+      </div>
+      <div className="grow">
+        <Card>form here</Card>
+        <Card>first post test</Card>
+      </div>
     </div>
   );
 }
