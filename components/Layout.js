10efa5c33b4e10edc2c11d1cce824acdbe683c54
Balint Korosi <balint.j.korosi@gmail.com>
2023-03-15 15:55:22 +0100

    Add login page

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,15 @@
 import Card from "@/components/Card";
 import NavigationCard from "@/components/NavigationCard";
 
-export default function Layout({ children }) {
+export default function Layout({ children, hideNavigation }) {
   return (
     <div className="flex max-w-4xl mx-auto mt-4 gap-6 ">
-      <div className="w-3/12">
-        <NavigationCard />
-      </div>
-      <div className="w-9/12">{children}</div>
+      {!hideNavigation && (
+        <div className="w-3/12">
+          <NavigationCard />
+        </div>
+      )}
+      <div className={hideNavigation ? "w-full" : "w-9/12"}>{children}</div>
     </div>
   );
 }
