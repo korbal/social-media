f3b6065951c38afb94f999022fdf669a167b73c7
Balint Korosi <balint.j.korosi@gmail.com>
2023-03-16 10:07:00 +0100

    yolo

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,24 @@ import Card from "@/components/Card";
 import NavigationCard from "@/components/NavigationCard";
 
 export default function Layout({ children, hideNavigation }) {
+  let rigthColumnClasses = "";
+
+  if (hideNavigation) {
+    rigthColumnClasses += "w-full";
+  } else {
+    rigthColumnClasses += "mx-4 md:mx-0 md:w-9/12";
+  }
+
   return (
-    <div className="flex max-w-4xl mx-auto mt-4 gap-6 ">
+    // left column
+    <div className="md:flex max-w-4xl mx-auto mt-4 gap-6 ">
       {!hideNavigation && (
-        <div className="w-3/12">
+        <div className="fixed md:static w-full bottom-0 md:w-3/12 -mb-5">
           <NavigationCard />
         </div>
       )}
-      <div className={hideNavigation ? "w-full" : "w-9/12"}>{children}</div>
+      {/* right column */}
+      <div className={rigthColumnClasses}>{children}</div>
     </div>
   );
 }
